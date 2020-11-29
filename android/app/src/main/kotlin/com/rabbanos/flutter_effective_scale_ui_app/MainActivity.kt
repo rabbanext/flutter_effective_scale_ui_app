d044ebbf137d1d1bf14178fb647af7b89af1149d
R Muhammad Rabbani P <rabbaniarvian@gmail.com>
2020-11-29 22:32:58 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/rabbanos/flutter_effective_scale_ui_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rabbanos.flutter_effective_scale_ui_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,302 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_effective_scale_ui_app/widgets/size_config.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scale UI to fit multiple display sizes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          padding: EdgeInsets.only(
+              left: SizeConfig.safeBlockHorizontal * 2,
+              top: SizeConfig.safeBlockVertical * 5,
+              right: SizeConfig.safeBlockHorizontal * 0,
+              bottom: SizeConfig.safeBlockVertical * 5
+          ),
+          children: [
+            SizedBox(height: SizeConfig.safeBlockVertical * 40,),
+            Row(
+              children: [
+                CupertinoButton(
+                  child: Container(
+                    height: SizeConfig.safeBlockVertical * 20,
+                    width: SizeConfig.safeBlockHorizontal * 40,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage('https://picsum.photos/250?image=9'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
+                      child: Text(
+                        "Mindfulness",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => TextScalePage()));
+                  },
+                ),
+                CupertinoButton(
+                  child: Container(
+                    height: SizeConfig.safeBlockVertical * 20,
+                    width: SizeConfig.safeBlockHorizontal * 40,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage('https://picsum.photos/250?image=9'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
+                      child: Text(
+                        "Mindfulness",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    showCupertinoDialog(
+                      context: context,
+                      builder: (BuildContext context) => CupertinoAlertDialog(
+                        title: const Text('Card is clicked.'),
+                        actions: <Widget>[
+                          CupertinoDialogAction(
+                            child: const Text('ok'),
+                            onPressed: () {
+                              Navigator.pop(context, 'ok');
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                CupertinoButton(
+                  child: Container(
+                    height: SizeConfig.safeBlockVertical * 20,
+                    width: SizeConfig.safeBlockHorizontal * 40,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage('https://picsum.photos/250?image=9'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
+                      child: Text(
+                        "Mindfulness",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    showCupertinoDialog(
+                      context: context,
+                      builder: (BuildContext context) => CupertinoAlertDialog(
+                        title: const Text('Card is clicked.'),
+                        actions: <Widget>[
+                          CupertinoDialogAction(
+                            child: const Text('ok'),
+                            onPressed: () {
+                              Navigator.pop(context, 'ok');
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                CupertinoButton(
+                  child: Container(
+                    height: SizeConfig.safeBlockVertical * 20,
+                    width: SizeConfig.safeBlockHorizontal * 40,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage('https://picsum.photos/250?image=9'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
+                      child: Text(
+                        "Mindfulness",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    showCupertinoDialog(
+                      context: context,
+                      builder: (BuildContext context) => CupertinoAlertDialog(
+                        title: const Text('Card is clicked.'),
+                        actions: <Widget>[
+                          CupertinoDialogAction(
+                            child: const Text('ok'),
+                            onPressed: () {
+                              Navigator.pop(context, 'ok');
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                CupertinoButton(
+                  child: Container(
+                    height: SizeConfig.safeBlockVertical * 20,
+                    width: SizeConfig.safeBlockHorizontal * 40,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage('https://picsum.photos/250?image=9'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
+                      child: Text(
+                        "Mindfulness",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    showCupertinoDialog(
+                      context: context,
+                      builder: (BuildContext context) => CupertinoAlertDialog(
+                        title: const Text('Card is clicked.'),
+                        actions: <Widget>[
+                          CupertinoDialogAction(
+                            child: const Text('ok'),
+                            onPressed: () {
+                              Navigator.pop(context, 'ok');
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                CupertinoButton(
+                  child: Container(
+                    height: SizeConfig.safeBlockVertical * 20,
+                    width: SizeConfig.safeBlockHorizontal * 40,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage('https://picsum.photos/250?image=9'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Container(
+                      margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
+                      child: Text(
+                        "Mindfulness",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {
+                    showCupertinoDialog(
+                      context: context,
+                      builder: (BuildContext context) => CupertinoAlertDialog(
+                        title: const Text('Card is clicked.'),
+                        actions: <Widget>[
+                          CupertinoDialogAction(
+                            child: const Text('ok'),
+                            onPressed: () {
+                              Navigator.pop(context, 'ok');
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextScalePage extends StatefulWidget {
+  @override
+  _TextScalePageState createState() => _TextScalePageState();
+}
+
+class _TextScalePageState extends State<TextScalePage> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(),
+      body: Center(
+        child: Text(
+          'Scaling text!',
+          style: TextStyle(
+            fontSize: SizeConfig.safeBlockHorizontal * 10,
+          ),
+        ),
+      ),
+    );
+  }
+}
